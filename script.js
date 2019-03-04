f4829285991d85760ed978f23dedf8be0a7cb585
leketemi <leketemi@gmail.com>
2019-03-04 02:47:58 -0800

    added menu to the header

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-var prompt = prompt('Are you saved?');
+//var prompt = prompt('Are you saved?');
+
+var lists = ['home', 'bible study', 'envangelism', 'about us', 'contact us'];
+var ul = document.querySelector('ul');
+
+lists.forEach(list => {
+	var li = document.createElement('li');
+	li.textContent = list;
+	ul.appendChild(li);
+});
