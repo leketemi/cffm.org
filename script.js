9a847417e81274cb75be3b24a93c444b5f82f41a
leketemi <leketemi@gmail.com>
2019-03-04 03:41:02 -0800

    added buttons for prompts

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-//var prompt = prompt('Are you saved?');
-
+var prompt;
 var lists = ['home', 'bible study', 'envangelism', 'about us', 'contact us'];
+
 var ul = document.querySelector('ul');
+var answer = document.querySelector('#answer');
+
+var buttons = Array.from(document.querySelectorAll('button'));
+buttons.forEach(button => {
+	button.addEventListener('click', (e) => {
+		if (button.id == 'save') {
+		prompt = prompt('Are you saved?');
+		answer.textContent = prompt;
+		}else if (button.id == 'holy') {
+			prompt = prompt('Are you holy?');
+			answer.textContent = prompt;
+		}else if (button.id == 'holy-spirit') {
+			prompt = prompt('Are you filled with holy spirit?');
+			answer.textContent = prompt;
+		}
+	});
+});
 
 lists.forEach(list => {
 	var li = document.createElement('li');
