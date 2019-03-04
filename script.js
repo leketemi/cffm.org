7197d382586eecbc2e1313a8976ff4d2865c1eae
leketemi <leketemi@gmail.com>
2019-03-04 04:03:34 -0800

    encapsulate code into functions

--- script.js
+++ script.js
@@ -4,24 +4,31 @@ var lists = ['home', 'bible study', 'envangelism', 'about us', 'contact us'];
 var ul = document.querySelector('ul');
 var answer = document.querySelector('#answer');
 
-var buttons = Array.from(document.querySelectorAll('button'));
-buttons.forEach(button => {
-	button.addEventListener('click', (e) => {
-		if (button.id === 'save') {
-		prompt = prompt('Are you saved?');
-		answer.textContent = prompt;
-		}else if (button.id === 'holy') {
-			prompt = prompt('Are you holy?');
-			answer.textContent = prompt;
-		}else if (button.id === 'holy-spirit') {
-			prompt = prompt('Are you filled with holy spirit?');
-			answer.textContent = prompt;
-		}
-	});
-});
+popUp();
+addLi();
 
-lists.forEach(list => {
-	var li = document.createElement('li');
-	li.textContent = list;
-	ul.appendChild(li);
-});
+function popUp(){
+	var buttons = Array.from(document.querySelectorAll('button'));
+	buttons.forEach(button => {
+		button.addEventListener('click', (e) => {
+			if (button.id === 'save') {
+			prompt = prompt('Are you saved?');
+			answer.textContent = prompt;
+			}else if (button.id === 'holy') {
+				prompt = prompt('Are you holy?');
+				answer.textContent = prompt;
+			}else if (button.id === 'holy-spirit') {
+				prompt = prompt('Are you filled with holy spirit?');
+				answer.textContent = prompt;
+			}
+		});
+	});
+}
+
+function addLi(){
+	lists.forEach(list => {
+		var li = document.createElement('li');
+		li.textContent = list;
+		ul.appendChild(li);
+	});
+}
